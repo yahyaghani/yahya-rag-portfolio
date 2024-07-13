c23f5a070b911a0c384568cb6d123bb05294cd9e
Yahya Ghani <yambox88@gmail.com>
2024-07-13 03:32:11 +0100

    updated-project-card

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -14,15 +14,23 @@ const ProjectCard = ({ project }) => {
         <div className="h-[1px] w-full bg-gradient-to-r from-transparent via-pink-500 to-violet-600"></div>
         <div className="h-[1px] w-full bg-gradient-to-r from-violet-600 to-transparent"></div>
       </div>
-      <div className="px-4 lg:px-8 py-3 lg:py-5 relative">
-        <div className="flex flex-row space-x-1 lg:space-x-2 absolute top-1/2 -translate-y-1/2">
-          <div className="h-2 w-2 lg:h-3 lg:w-3 rounded-full bg-red-400"></div>
-          <div className="h-2 w-2 lg:h-3 lg:w-3 rounded-full bg-orange-400"></div>
-          <div className="h-2 w-2 lg:h-3 lg:w-3 rounded-full" style={{ backgroundColor: 'green' }}></div>
+      <div className="px-4 lg:px-8 py-3 lg:py-5 relative flex items-center justify-between">
+        <div className="flex items-center space-x-2">
+          <div className="flex flex-row space-x-1 lg:space-x-2">
+            <div className="h-2 w-2 lg:h-3 lg:w-3 rounded-full bg-red-400"></div>
+            <div className="h-2 w-2 lg:h-3 lg:w-3 rounded-full bg-orange-400"></div>
+            <div className="h-2 w-2 lg:h-3 lg:w-3 rounded-full" style={{ backgroundColor: 'green' }}></div>
+          </div>
+          <p className="text-center ml-3 text-[#16f2b3] text-base lg:text-xl flex-grow text-center">
+            {project.name}
+          </p>
         </div>
-        <p className="text-center ml-3 text-[#16f2b3] text-base lg:text-xl">
-          {project.name}
-        </p>
+        <div className="ml-auto">
+          <button onClick={nextPage} className="flex items-center px-4 py-2 bg-gradient-to-r from-green-400 to-blue-500 text-white rounded-full">
+            <span>learn more</span>
+            <FaArrowRight className="ml-2" />
+          </button>
+        </div>
       </div>
       <div className="overflow-hidden border-t-[2px] border-indigo-900 px-4 lg:px-8 py-4 lg:py-8">
         {page === 1 && (
@@ -98,12 +106,6 @@ const ProjectCard = ({ project }) => {
             )}
           </div>
         )}
-        <div className="flex justify-center mt-4">
-          <button onClick={nextPage} className="flex items-center px-4 py-2 bg-gradient-to-r from-green-400 to-blue-500 text-white rounded-full">
-            <span>learn more</span>
-            <FaArrowRight className="ml-2" />
-          </button>
-        </div>
       </div>
     </div>
   );
